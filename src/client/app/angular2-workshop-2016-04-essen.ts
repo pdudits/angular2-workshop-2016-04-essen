c45f446a54b80e6faff7a397b77a09188e348997
Elmar Burke <elmar.burke@symetics.com>
2016-04-20 10:29:43 +0200

    router

--- src/client/app/angular2-workshop-2016-04-essen.ts
+++ src/client/app/angular2-workshop-2016-04-essen.ts
@@ -1,17 +1,28 @@
-import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {Component, provide} from 'angular2/core';
+import {
+  RouteConfig,
+  ROUTER_DIRECTIVES,
+  ROUTER_PROVIDERS,
+  LocationStrategy,
+  HashLocationStrategy
+} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {CliRouteConfig} from './route-config';
 import {BookIndex} from './book-index/book-index';
 @Component({
   selector: 'angular2-workshop-2016-04-essen-app',
-  providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS],
+  providers: [
+    ROUTER_PROVIDERS,
+    HTTP_PROVIDERS,
+    provide(LocationStrategy, {
+      useClass: HashLocationStrategy
+    })],
   templateUrl: 'app/angular2-workshop-2016-04-essen.html',
   directives: [ROUTER_DIRECTIVES, BookIndex],
   pipes: []
 })
 @RouteConfig([
-
+  {path: '/books', component: BookIndex, useAsDefault: true}
 ].concat(CliRouteConfig))
 
 export class Angular2Workshop201604EssenApp {
